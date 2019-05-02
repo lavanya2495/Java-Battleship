6837bcd4cbed8498e377913cc2cacb01e222c61e
Scott Moser <mosers1@seattleu.edu>
2019-05-01 19:29:42 -0700

    Adding template for Location unit tests.

---
+++ LocationTest.java
@@ -0,0 +1,116 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LocationTest
+{
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    void testLocation()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testCheckHit()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testCheckMiss()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsUnguessed()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testMarkHit()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testMarkMiss()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testHasShip()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetShip()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetStatus()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetStatus()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetLengthOfShip()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetLengthOfShip()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetDirectionOfShip()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetDirectionOfShip()
+    {
+        fail("Not yet implemented");
+    }
+
+}
