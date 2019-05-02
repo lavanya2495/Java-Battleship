7a421fef8a4716945bbe6f8948e028d8c7da2e95
DESKTOP-66HMKDP\nicho <benyon@seattleu.edu>
2019-05-02 04:20:38 -0700

    Create ShipTest.java
    Added unit tests

---
+++ ShipTest.java
@@ -0,0 +1,98 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ShipTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testShip() {
+		Ship testShip = new Ship(2);
+		assertEquals(testShip.getDirection(), -1);
+		assertEquals(testShip.getLength(), 2);
+		assertEquals(testShip.getRow(), -1);
+		assertEquals(testShip.getCol(), -1);
+	}
+
+	@Test
+	void testIsLocationSet() {
+		Ship testShip = new Ship(2);
+		assertEquals(testShip.isLocationSet(), false);
+		testShip.setLocation(0, 0);
+		assertEquals(testShip.isLocationSet(), true);
+	}
+
+	@Test
+	void testIsDirectionSet() {
+		Ship testShip = new Ship(2);
+		assertEquals(testShip.isDirectionSet(), false);
+		testShip.setDirection(1);
+		assertEquals(testShip.isDirectionSet(), true);
+		assertThrows(IllegalArgumentException.class, () -> {
+	        testShip.setDirection(2);
+	    });
+		assertThrows(IllegalArgumentException.class, () -> {
+	        testShip.setDirection(-2);
+	    });
+	}
+
+	@Test
+	void testSetLocation() {
+		Ship testShip = new Ship(2);
+		testShip.setLocation(0, 0);
+		assertEquals(testShip.getRow(), 0);
+		assertEquals(testShip.getCol(), 0);
+	}
+
+	@Test
+	void testSetDirection() {
+		Ship testShip = new Ship(2);
+		testShip.setDirection(0);
+		assertEquals(testShip.getDirection(), 0);
+	}
+
+	@Test
+	void testGetRow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
