218dc5904a19df86bdb9557b8358709fa7cbe941
Scott Moser <mosers1@seattleu.edu>
2019-05-28 23:32:15 -0700

    Improved parsing of test suite results vs. individual test case results.

--- scripts/runTestSuite.sh
+++ scripts/runTestSuite.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #===============================================================================
-#!/bin/bash
-#===============================================================================
 # Team D'Buggers (Team 7)
 # Professor McKee
 # CPSC 5200-01
@@ -18,9 +16,9 @@
 #===============================================================================
 
 #set -o errexit
-#set -o pipefail
+set -o pipefail
 set -o nounset
-# set -o xtrace
+#set -o xtrace
 
 #===============================================================================
 # Constants
@@ -61,14 +59,16 @@ fi
 rm -f $LOG_FILE
 
 # Run the test suite
-numTests=$1
+numIter=$1
 numPass=0
 numFail=0
 echo "Running the test suite..." | tee -a $LOG_FILE
 echo "Start date/time:" $(date) | tee -a $LOG_FILE
 start=$SECONDS
-for i in `seq 1 $numTests`; do
+for i in `seq 1 $numIter`; do
+    echo "****************************************" | tee -a $LOG_FILE
     echo "Executing test run $i of $1..." | tee -a $LOG_FILE
+    echo "****************************************" | tee -a $LOG_FILE
     java $CS1_HACK -jar $PROJECT_ROOT/$JUNIT_JAR --class-path $PROJECT_ROOT/$OUT_DIR --scan-class-path | tee -a $LOG_FILE
     rv=$?
     echo "Tests completed with return code: $rv" | tee -a $LOG_FILE
@@ -78,20 +78,49 @@ for i in `seq 1 $numTests`; do
         let "numFail++"
     fi
 done
-stop=$(( $SECONDS - $start ))
+stop=$SECONDS
 echo "Stop date/time:" $(date) | tee -a $LOG_FILE
 
+# Calculate statistics
+  # Total execution time
+duration=$(( $stop - $start ))
+  # Search log file for the number of test cases per test suite run
+numCases=$(grep -i -m1 "tests found" $LOG_FILE | grep -o -E '[0-9]+')
+  # Multiply test cases per run times the number of runs
+numCases=$((numCases * numIter))
+  # Search the log file for the number of failing test cases
+numCaseFail=$(grep -i -c "AssertionFailedError" $LOG_FILE)
+  # The remainder are passing tests
+numCasePass=$((numCases - numCaseFail))
+  # Calculate the individual test case passing rate
+testCasePassRate=$(bc -l <<< "scale=2; $numCasePass/$numCases*100")
+  # Calculate the test suite passing rate
+testSuitePassRate=$(bc -l <<< "scale=2; $numPass/$numIter*100")
+  
 # Report statistics
-echo "Test duration:       $stop [seconds]" | tee -a $LOG_FILE
-echo "Number of test runs: $numTests" | tee -a $LOG_FILE
-echo "Number of PASS:      $numPass"  | tee -a $LOG_FILE
-echo "Number of FAIL:      $numFail"  | tee -a $LOG_FILE
-echo "Passing rate:        $((numPass / numTests * 100 ))%" | tee -a $LOG_FILE
+echo ""
+echo "================================================================================" | tee -a $LOG_FILE
+echo "Statistics" | tee -a $LOG_FILE
+echo "================================================================================" | tee -a $LOG_FILE
+echo "Execution time:           $duration [seconds]"     | tee -a $LOG_FILE
+echo ""                                                  | tee -a $LOG_FILE
+echo "# of test suite runs:     $numIter"                | tee -a $LOG_FILE
+echo "# of PASSing runs:        $numPass"                | tee -a $LOG_FILE
+echo "# of FAILing runs:        $numFail"                | tee -a $LOG_FILE
+echo "Test suite passing rate:  $testSuitePassRate%"     | tee -a $LOG_FILE
+echo ""                                                  | tee -a $LOG_FILE
+echo "# of test cases run:      $numCases"               | tee -a $LOG_FILE
+echo "# of PASSing test cases:  $numCasePass"            | tee -a $LOG_FILE
+echo "# of FAILing test cases:  $numCaseFail"            | tee -a $LOG_FILE
+echo "Test case passing rate:   $testCasePassRate%"      | tee -a $LOG_FILE
+echo "================================================================================" | tee -a $LOG_FILE
+echo "See the log file for the full console output: $LOG_FILE"
+echo ""
 
 # Determine overall result
 isFail=1
 status="FAIL"
-if [ $numPass == $numTests ]; then
+if [ $numPass == $numIter ]; then
     isFail=0
     status="PASS"
 fi
@@ -107,7 +136,7 @@ if [ $# == $MAX_ARGS ]; then
 fi
 
 # Set return code
-if [ $numPass == $numTests ]; then
+if [ $numPass == $numIter ]; then
     exit 0
 else
     exit 4
